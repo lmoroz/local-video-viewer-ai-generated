ed54d7160ea06cde3625010deb254f2041632529
Larisa Morozhnikova <larisa.moroz@gmail.com>
2025-12-03 00:52:24 +0800

    add scrollBehavior to router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,35 +1,46 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import HomePage from '../views/HomePage.vue';
-import PlaylistPage from '../views/PlaylistPage.vue';
-import VideoPage from '../views/VideoPage.vue';
+import { createRouter, createWebHistory } from 'vue-router'
+import HomePage from '../views/HomePage.vue'
+import PlaylistPage from '../views/PlaylistPage.vue'
+import VideoPage from '../views/VideoPage.vue'
 
 const routes = [
-    {
-        path: '/',
-        name: 'Home',
-        component: HomePage,
-    },
-    {
-        path: '/playlist/:name',
-        name: 'Playlist',
-        component: PlaylistPage,
-        props: (route) => ({ name: route.params.name, dir: route.query.dir }),
-    },
-    {
-        path: '/video/:filename',
-        name: 'Video',
-        component: VideoPage,
-        props: (route) => ({
-            filename: route.params.filename,
-            dir: route.query.dir,
-            playlist: route.query.playlist
-        }),
-    },
-];
+  {
+    path: '/',
+    name: 'Home',
+    component: HomePage
+  },
+  {
+    path: '/playlist/:name',
+    name: 'Playlist',
+    component: PlaylistPage,
+    props: route => ({ name: route.params.name, dir: route.query.dir })
+  },
+  {
+    path: '/video/:filename',
+    name: 'Video',
+    component: VideoPage,
+    props: route => ({
+      filename: route.params.filename,
+      dir: route.query.dir,
+      playlist: route.query.playlist
+    })
+  }
+]
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
-});
+  history: createWebHistory(),
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (to.hash) {
+      return {
+        el: to.hash
+      }
+    } else if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
+  }
+})
 
-export default router;
+export default router
